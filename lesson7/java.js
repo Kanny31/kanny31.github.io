1390842123e2fc5f3730181df3dd515a0ed91953
Kanny31 <aka18001@byui.edu>
2019-10-31 21:57:06 -0600

    upload

--- lesson7/java.js
+++ lesson7/java.js
@@ -1,41 +1,3 @@
-//get all imgs with data-src attribute
-const imagesToLoad = document.querySelectorAll('img[data-src]')
-//parameters set for the IntersectionObserver
-const imgOptions = {
-    threshold: 0,
-    rootMargin: "10px 10px 50px 10px"
-};
-
-const loadImages = (image) => {
-    image.setAttribute('src', image.getAttribute('data-src'));
-    image.onload = () => {
-        image.removeAttribute('data-src');
-        };
-    };
-
-if ('IntersectionObserver' in window) {
-    const observer = new IntersectionObserver ((items, observer) => {
-        items.forEach((item) => {
-            if(item.isIntersecting) {
-                loadImages(item.target);
-                observer.unobserve(item.target);
-            }
-          });
-        }, imgOptions);
-//loop through images, load if necessary
-imagesToLoad.forEach((img) => {
-    observer.observe(img);
-});
-}   else{ //load all images if not supported
-    imagesToLoad.forEach((img) => {
-        loadImages(img);
-    });
-}
-
-
-
-
-
 var imagesToLoad = document.querySelectorAll("img[data-src]");
 var loadImages = function(image) {
 	image.setAttribute("src", image.getAttribute("data-src"));
@@ -62,4 +24,4 @@ else {
 	});
 }
 
-/!citation: help from classmate McKenzie Lewis!/ 
+/!citation: help from classmate Jonathan lundquist !/ 
