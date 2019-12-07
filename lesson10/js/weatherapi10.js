5c3c77eb6807c4567ca9790a0fca32fb019118d6
Kanny31 <aka18001@byui.edu>
2019-12-06 22:45:57 -0700

    upload

--- lesson10/js/weatherapi10.js
+++ lesson10/js/weatherapi10.js
@@ -17,7 +17,7 @@ fetch(apiURL)
 
   });
 
-  const jsonURL = "https://api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=bd99fbf52e995e4af1074e56feb731ab";
+  const jsonURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=f59e1a32a18fcdcb1bcc9c2905156e33";
 
   fetch(jsonURL)
     .then((response) => response.json())
